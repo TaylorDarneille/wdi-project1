0571b765cf8d7a4ac15d8199092a2929623f82ef
TaylorDarneille <tmdarneille@dons.usfca.edu>
2017-12-11 12:26:49 -0800

    finished setting up board with loadBoard function

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-var whiterook = '\u2656';
-var r1c1 = document.getElementsByClassName("row1 col1")[0];
+var whiteRook = "\u2656";
+var whiteKnight = "\u2658";
+var whiteBishop = "\u2657";
+var whiteQueen = "\u2655";
+var whiteKing = "\u2654";
+var whitePawn = "\u2659";
+var blackRook = "\u265C";
+var blackKnight = "\u265E";
+var blackBishop = "\u265D";
+var blackQueen = "\u265B";
+var blackKing = "\u265A";
+var blackPawn = "\u265F";
+var row1 = document.getElementsByClassName("row1");
+var row2 = document.getElementsByClassName("row2");
+var row7 = document.getElementsByClassName("row7");
+var row8 = document.getElementsByClassName("row8");
 
 
 function loadBoard() {
-	r1c1.textContent=whiterook;
+	row1[0].textContent = whiteRook;
+	row1[7].textContent = whiteRook;
+	row1[1].textContent = whiteKnight;
+	row1[6].textContent = whiteKnight;
+	row1[2].textContent = whiteBishop;
+	row1[5].textContent = whiteBishop;
+	row1[3].textContent = whiteQueen;
+	row1[4].textContent = whiteKing;
+	for(var i=0; i<8; i++) {
+		row2[i].textContent = whitePawn;
+	}
+
+	row8[0].textContent = blackRook;
+	row8[7].textContent = blackRook;
+	row8[1].textContent = blackKnight;
+	row8[6].textContent = blackKnight;
+	row8[2].textContent = blackBishop;
+	row8[5].textContent = blackBishop;
+	row8[3].textContent = blackQueen;
+	row8[4].textContent = blackKing;
+	for(var i=0; i<8; i++) {
+		row7[i].textContent = blackPawn;
+	}
 }
 
 document.addEventListener("DOMContentLoaded", function(){
